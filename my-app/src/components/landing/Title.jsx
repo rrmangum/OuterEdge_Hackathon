3973bdcf82c46154539796ff68ff73ef4f10e6a1
blueheelerco <chris@starfirevp.com>
2023-03-18 16:57:36 -0700

    pull main

--- my-app/src/components/landing/Title.jsx
+++ my-app/src/components/landing/Title.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import Loki from "react-loki";
+import { BsFillHouseDoorFill } from "react-icons/bs";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { CompanyName } from "../../assets/content/content";
+import Default from "../../layouts/Default";
 import logo from "../../assets/images/logos/green-flame.png";
 import "./landing.css";
+
+const customSteps = [
+  {
+    label: "Add Listing Information",
+    icon: <BsFillHouseDoorFill />,
+    element: <Default />,
+  },
+];
 
 export default function Title() {
   return (
@@ -14,6 +25,14 @@ export default function Title() {
           </Col>
           <Col className="px-0 mx-0">
             <div className="text-white landing-title">{CompanyName}&trade;</div>
+          </Col>
+          <Col>
+            <Loki
+              steps={customSteps}
+              backLabel={"Back"}
+              nextLabel={"Next"}
+              noActions
+            />
           </Col>
         </Row>
       </Container>
