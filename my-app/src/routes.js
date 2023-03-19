903c5aaae17cc501d2374ac6f786cb8b6f380dff
Ryan Mangum <rrmangum@gmail.com>
2023-03-18 20:06:24 -0700

    web3 connection

--- my-app/src/routes.js
+++ my-app/src/routes.js
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 
 const Landing = lazy(() => import("./components/landing/Landing"));
+const Creator = lazy(() => import("./components/creator/Creator"))
 
 const routes = [
   {
@@ -8,6 +9,13 @@ const routes = [
     name: "Landing",
     exact: true,
     element: Landing,
+    isAnonymous: true,
+  },
+  {
+    path: "/creator",
+    name: "Creator",
+    exact: true,
+    element: Creator,
     isAnonymous: true,
   },
 ];
